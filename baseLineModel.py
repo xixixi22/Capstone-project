ce36d99ddd46a9349ad86d382be9ebf434214a8f
xixixi22 <54451222+xixixi22@users.noreply.github.com>
2020-12-09 09:26:20 -0500

    Update baseLineModel.py

--- baseLineModel.py
+++ baseLineModel.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import numpy as np
-from datetime import datetime
 from sklearn import linear_model
-from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
 from sklearn.multioutput import MultiOutputRegressor
 from scipy.stats import pearsonr, spearmanr
-from sklearn.metrics import mean_squared_error
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import ElasticNet
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.linear_model import LassoCV
+from sklearn.linear_model import RidgeCV
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
+
 def correlation(label_test, label_predict, correlation_type):
     if correlation_type == 'pearson':
         corr = pearsonr
@@ -19,49 +23,35 @@ def correlation(label_test, label_predict, correlation_type):
     for lb_test, lb_predict in zip(label_test, label_predict):
         score.append(corr(lb_test, lb_predict)[0])
     return np.mean(score), score
-def precision_k(label_test, label_predict, k):
-    num_pos = 100
-    num_neg = 100
-    label_test = np.argsort(label_test, axis=1)
-    label_predict = np.argsort(label_predict, axis=1)
-    precision_k_neg = []
-    precision_k_pos = []
-    neg_test_set = label_test[:, :num_neg]
-    pos_test_set = label_test[:, -num_pos:]
-    neg_predict_set = label_predict[:, :k]
-    pos_predict_set = label_predict[:, -k:]
-    for i in range(len(neg_test_set)):
-        neg_test = set(neg_test_set[i])
-        pos_test = set(pos_test_set[i])
-        neg_predict = set(neg_predict_set[i])
-        pos_predict = set(pos_predict_set[i])
-        precision_k_neg.append(len(neg_test.intersection(neg_predict)) / k)
-        precision_k_pos.append(len(pos_test.intersection(pos_predict)) / k)
-    return np.mean(precision_k_neg), np.mean(precision_k_pos)
 
-
-def rmse(label_test, label_predict):
-    return np.sqrt(mean_squared_error(label_test, label_predict))
 
 def print_scores(y_array,pred_y):
     pearson_mean,_ = correlation(y_array, pred_y, 'pearson')
     spearman_mean,_ = correlation(y_array, pred_y, 'spearman')
     print(pearson_mean,spearman_mean)
     print(rmse(y_array, pred_y))
-    for k in precision_degree:
-        precision_neg, precision_pos = precision_k(y_array, pred_y, k)
-        print("Precision@%d Positive: %.4f" % (k, precision_pos))
-        print("Precision@%d Negative: %.4f" % (k, precision_neg))
+    
 
-def knn(x):
+def knn(x,test):
     neigh = KNeighborsRegressor(n_neighbors=x)
     regr_multiknn= MultiOutputRegressor(neigh).fit(train_X,train_y)
-    test_pre = regr_multiknn.predict(test_X)
-    print_scores(test_y_array,test_pre)
-    print("dev:============")
-    dev_pre = regr_multiknn.predict(dev_X)
-    print_scores(dev_y_array, dev_pre)
+    multiknn_pre = regr_multiknn.predict(test)
+    print_score(test,multiknn_pre)
+    
+def randomSearch(base_model,random_grid):
+    random = RandomizedSearchCV(MultiOutputRegressor(base_model),param_distributions = random_grid,
+                               n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
 
+    random.fit(train_X, train_y)
+    print(random.best_params_)
+    best_random = random.best_estimator_
+    pred_y_train = best_random.predict(train_X)
+    print_scores(train_y_array,pred_y_train)
+    pred_y_test = best_random.predict(test_X)
+    print_scores(test_y_array,pred_y_test)
+    pred_y_dev = best_random.predict(dev_X)
+    print_scores(dev_y_array,pred_y_dev)
+    
 train = pd.DataFrame(pd.read_csv("data/train.csv"))
 test = pd.DataFrame(pd.read_csv("data/test.csv"))
 dev = pd.DataFrame(pd.read_csv("data/dev.csv"))
@@ -73,32 +63,64 @@ dev_X,dev_y=dev[dev.columns[978:]],dev[dev.columns[:978]]
 test_y_array=test_y.to_numpy()
 dev_y_array=dev_y.to_numpy()
 print(train_X)
-precision_degree = [10, 20, 50, 100]
 
-# for i in [0.7,0.8,0.9]:
-#     clf = linear_model.Lasso(alpha=i)
-#     start_time = datetime.now()
-#     multi_lasso = MultiOutputRegressor(clf).fit(train_X,train_y)
-#     print("================",i)
-#     print("test")
-#     l_test=multi_lasso.predict(test_X)
-#     print("dev")
-#     l_dev = multi_lasso.predict(dev_X)
-#
-#     print_scores(test_y_array,l_test)
-#     print_scores(dev_y_array,l_dev)
-#     end_time = datetime.now()
-#     print(end_time - start_time)
-for i in range(10，20):
-    clf = linear_model.Ridge(alpha=i)
-    start_time = datetime.now()
-    multi_ridge = MultiOutputRegressor(clf).fit(train_X, train_y)
-    print("================", i)
-    print("test")
-    l_test = multi_ridge.predict(test_X)
-    print("dev")
-    l_dev = multi_ridge.predict(dev_X)
-    print_scores(test_y_array, l_test)
-    print_scores(dev_y_array, l_dev)
-    end_time = datetime.now()
-    print(end_time - start_time)
+
+
+#random forest with random search
+print("++++++++random search RF+++++++")
+regr_RF = RandomForestRegressor()
+n_estimators = [10,50,100,150]
+max_depth = [10,15,20]
+max_leaf_nodes = [3,5,7,9]
+max_features=["auto", "sqrt", "log2"]
+random_grid_RF = {
+    'estimator__n_estimators':n_estimators,
+    'estimator__max_depth':max_depth,
+    'estimator__max_leaf_nodes':max_leaf_nodes,
+    'estimator__max_features':max_features
+}
+randomSearch(regr_RF,random_grid_RF)
+
+#elastic net with random search
+regr_EN = ElasticNet()
+alpha = [x for x in np.linspace(0.01,1,5)]
+l1_ratio = [x for x in np.linspace(0.01,1,5)]
+
+random_grid = {
+    'estimator__alpha':alpha,
+    'estimator__l1_ratio':l1_ratio
+}
+randomSearch(regr_EN,random_grid)
+
+#knn
+for i in range(1,19):
+    knn(i,test_X)
+    
+#lassoCV 
+alphas=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+regr_multila = MultiOutputRegressor(LassoCV(cv = 5,alphas=alphas))
+regr_multila.fit(train_X,train_y)
+regr_multila.get_params
+y_pred = regr_multila.predict(test_X)
+print_scores(test_y_array,y_pred)
+
+#ridgeCV
+regr_multiri = MultiOutputRegressor(RidgeCV(cv = 5,alphas = alphas))
+regr_multiri.fit(train_X,train_y)
+regr_multiri.get_params
+y_pred = regr_multiri.predict(test_X)
+print_scores(test_y_array,y_pred)
+
+#ridge
+for i in alphas:
+    regr_multir = MultipleOutputRegressor(Ridge(alpha = i))
+    regr_multir.fit(train_X,train_y)
+    y_pred1 = regr_multir.predict(test_X)
+    print_scores(test_y_array,y_pred1)
+    
+# lasso
+for i in alphas:
+    regr_multir = MultipleOutputRegressor(lasso(alpha = i))
+    regr_multir.fit(train_X,train_y)
+    y_pred = regr_multir.predict(test_X)
+    print_scores(test_y_array,y_pred)
